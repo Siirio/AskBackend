ff774021b4fa7c2e438e6e3c6a746c226ff5605a
Anvar <lolopenza@gmail.com>
2026-06-23 16:26:31 +0500

    Revert "add service basic logic"
    This reverts commit 0be6876795832d9be19a2eeacacbf6408213308c.

--- src/main/java/kz/ask/service/domain/enums/BookingStatus.java
+++ src/main/java/kz/ask/service/domain/enums/BookingStatus.java
@@ -2,10 +2,7 @@ package kz.ask.service.domain.enums;
 
 public enum BookingStatus {
     PENDING,
-    CONFIRMED_BY_USER,
-    COMPLETED_BY_USER,
-    CANCELLED_BY_USER,
-    CONFIRMED_BY_BUSINESS,
-    DECLINED_BY_BUSINESS,
-    SUGGEST_OTHER_TIME_BY_BUSINESS
+    CONFIRMED,
+    COMPLETED,
+    CANCELLED
 }
